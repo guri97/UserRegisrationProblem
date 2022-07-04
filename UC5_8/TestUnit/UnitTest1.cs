7320f005a2f1bde94a90a70b9f6bab5d39c77306
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-04 13:57:26 +0530

    [gurpreet] UC5_8PasswordValidation

---
+++ UC5_8/TestUnit/UnitTest1.cs
@@ -0,0 +1,91 @@
+using UserRegistrationTestAndRegex;
+
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationTest
+{
+    public class Tests
+    {
+        UserRegistrationDetails user;
+        [SetUp]
+        public void Setup()
+        {
+            user = new UserRegistrationDetails();
+        }
+
+        [Test]
+        public void FirstName()
+        {
+            // Arrange 
+            bool expected = true;
+            string name = "Gurpreet";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateFirstName(name);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void LastName()
+        {
+            // Arrange 
+            bool expected = true;
+            string name = "Singh";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateLastName(name);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Email()
+        {
+            // Arrange 
+            bool expected = true;
+            string email = "gsgurpreet766@gmail.com";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void MobileNumber()
+        {
+            // Arrange 
+            bool expected = true;
+            string mobile = "91 9531812464";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateMobileNumber(mobile);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Password()
+        {
+            // Arrange 
+            bool expected = true;
+            string password = "abcdfghihgE1@";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+
+
+
+}
