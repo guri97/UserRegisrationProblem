155c9eef67678e787fd98ea6403967b316de537a
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-04 12:36:47 +0530

    [gurpreet] UC1_FirstName

---
+++ UserRegistrationProblem/TestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+using UserRegistrationTestAndRegex;
+
+namespace UserRegistrationTest
+{
+    public class Tests
+    {
+        UserRegistrationDetails user;
+        [SetUp]
+        public void Setup()
+        {
+            user = new UserRegistrationDetails();
+        }
+
+        [Test]
+        public void FirstName()
+        {
+            // Arrange 
+            bool expected = true;
+            string name = "Gurpreet";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateFirstName(name);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
