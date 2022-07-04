a95d8d7ebb4c08e166f5be2311e72f6ee8f581c4
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-04 13:10:12 +0530

    [gurpreet] UC4_ValidationofMobileNumber

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -28,14 +28,18 @@
             string lastName = Console.ReadLine();
             Console.Write("Email:     ");
             string email = Console.ReadLine();
+            Console.Write("Mobile Number:     ");
+            string mobile = Console.ReadLine();
             Console.Write("\n=====Result======\n");
             bool checkFirstName = user.ValidateFirstName(firstName);
             bool checkLastName = user.ValidateLastName(lastName);
             bool checkEmail = user.ValidateEmail(email);
+            bool checkMobileNumber = user.ValidateMobileNumber(mobile);
             Console.WriteLine("\n{0}\n{1}\n{2}\n{3}\n{4} ",
             (checkFirstName) ? "First name is valid" : "First name is invalid",
             (checkLastName) ? "Last name is valid" : "Last name is invalid",
-            (checkEmail) ? "Email address is valid" : "Email address is invalid");
+            (checkEmail) ? "Email address is valid" : "Email address is invalid",
+            (checkMobileNumber) ? "Mobile number is valid" : "Mobile number is invalid");
 
         }
     }
